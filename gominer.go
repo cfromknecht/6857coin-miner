c5306e04a723cb7512042344029cbcf2d50abd40
Douglas Chen <dpchen@mit.edu>
2016-02-23 21:20:45 -0500

    initial commit

---
+++ gominer.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"hash"
+	"log"
+	"math/rand"
+	"net/http"
+	"reflect"
+	"sync"
+	"time"
+)
+
+func main() {
+	mine()
+}
+
+func mine() error {
+	resp, err := http.Get("http://6857coin.csail.mit.edu:8080/next")
+	if err != nil {
+		return err
+	}
+	dec := json.NewDecoder(resp.Body)
+	blk := Block {
+		Header: new(BlockHeader),
+		Block: "dpchen,lopezv,asnoakes",
+	}
+	dec.Decode(blk.Header)
+	blk.Header.Timestamp = uint64(time.Now().UnixNano())
+	blk.setRoot()
+
+	log.Println(blk.Header)
+
+	col := newCollider(blk.Header)
+	blk.Header.Nonces = col.collide()
+
+	encblk, err := json.Marshal(blk)
+	if err != nil {
+		return err
+	}
+
+	resp, err = http.Post("http://6857coin.csail.mit.edu:8080/add", "application/json", bytes.NewBuffer(encblk))
+	log.Println(resp, err)
+	return err
+}
+
+func mask(difficulty uint64) uint64 {
+	return (1 << difficulty) - 1;
+}
+
+type BlockHeader struct {
+	ParentId	string `json:'parentid'`
+	Root	string `json:'root'`
+	Difficulty uint64 `json:'difficulty'`
+	Timestamp uint64 `json:'timestamp'`
+	Nonces []uint64 `json:'nonces'`
+	Version byte `json:'byte'`
+}
+
+type Block struct {
+	Header *BlockHeader `json:'header'`
+	Block string `json:'block'`
+}
+
+func (b *Block) setRoot() {
+	sum := sha256.Sum256([]byte(b.Block))
+	b.Header.Root = hex.EncodeToString(sum[:])
+}
+
+func (b *BlockHeader) prefixHash() hash.Hash {
+	h := sha256.New()
+	parentId, _ := hex.DecodeString(b.ParentId)
+	h.Write(parentId)
+	root, _ := hex.DecodeString(b.Root)
+	h.Write(root)
+	binary.Write(h, binary.BigEndian, b.Difficulty)
+	binary.Write(h, binary.BigEndian, b.Timestamp)
+	return h
+}
+
+func copyHash(src hash.Hash) hash.Hash {
+	typ := reflect.TypeOf(src)
+	val := reflect.ValueOf(src)
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+		val = val.Elem()
+	}
+	elem := reflect.New(typ).Elem()
+	elem.Set(val)
+	return elem.Addr().Interface().(hash.Hash)
+}
+
+func (b *BlockHeader) suffixHash(buf []byte, h hash.Hash, nonce uint64) uint64 {
+	hh := copyHash(h)
+	binary.BigEndian.PutUint64(buf, nonce)
+	buf[8] = b.Version
+	hh.Write(buf)
+	sum := hh.Sum(nil)
+	return binary.BigEndian.Uint64(sum[len(sum)-8:])
+}
+
+func (b *BlockHeader) doHash(nonce uint64) {
+	h := sha256.New()
+	parentId, _ := hex.DecodeString(b.ParentId)
+	h.Write(parentId)
+	root, _ := hex.DecodeString(b.Root)
+	h.Write(root)
+	binary.Write(h, binary.BigEndian, b.Difficulty)
+	binary.Write(h, binary.BigEndian, b.Timestamp)
+
+	buf := make([]byte, 9)
+	binary.BigEndian.PutUint64(buf, nonce)
+	buf[8] = b.Version
+	h.Write(buf)
+
+	log.Println(hex.EncodeToString(h.Sum(nil)))
+}
+
+type BucketEntry struct {
+	nonce uint64
+	sum uint64
+}
+
+type Collider struct {
+	tableMask uint64
+	buckets [][]BucketEntry
+	locks []sync.Mutex
+	header *BlockHeader
+}
+
+func newCollider(h *BlockHeader) *Collider {
+	size := (1 << (h.Difficulty * 2 / 3 + 1))
+	log.Println("collider allocating", size)
+	return &Collider{
+		tableMask: uint64(size - 1),
+		buckets: make([][]BucketEntry, size),
+		locks: make([]sync.Mutex, 256),
+		header: h,
+	}
+}
+
+func (c *Collider) insert(sum uint64, nonce uint64) (n int) {
+	bucket := sum & c.tableMask
+	lock := sum & 255
+	c.locks[lock].Lock()
+	n = 1
+	for _, v := range c.buckets[bucket] {
+		if v.nonce == nonce {
+			c.locks[lock].Unlock()
+			return
+		}
+		if v.sum == sum {
+			n++
+		}
+	}
+	c.buckets[bucket] = append(c.buckets[bucket], BucketEntry{nonce: nonce, sum: sum})
+	c.locks[lock].Unlock()
+	return
+}
+
+func (c *Collider) getAll(sum uint64) (nonces []uint64) {
+	bucket := sum & c.tableMask
+	lock := sum & 255
+	c.locks[lock].Lock()
+	for _, v := range c.buckets[bucket] {
+		if v.sum == sum {
+			nonces = append(nonces, v.nonce)
+		}
+	}
+	c.locks[lock].Unlock()
+	return
+}
+
+func (c *Collider) collideWorker(res chan uint64, stop chan bool, progress chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	origH := c.header.prefixHash()
+	nonce := uint64(rand.Int63())
+	m := mask(c.header.Difficulty)
+	buf := make([]byte, 9)
+	for i := 0; ; i++ {
+		sum := c.header.suffixHash(buf, origH, nonce) & m
+		if c.insert(sum, nonce) >= 3 {
+			log.Println("found sum", sum)
+			select {
+			case res <- sum:
+			case <-stop:
+			}
+			return
+		}
+		nonce++
+
+		if i > 0 && i % 1000000 == 0 {
+			progress <- true
+		}
+		if i & 65535 == 0 {
+			select {
+			case <-stop:
+				return
+			default:
+			}
+		}
+	}
+}
+
+func (c *Collider) collide() (nonces []uint64) {
+	workers := 12
+
+	log.Println("starting workers")
+	res := make(chan uint64)
+	progress := make(chan bool)
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+	stop := make(chan bool)
+	defer close(stop)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go c.collideWorker(res, stop, progress, &wg)
+	}
+
+	count := 0
+	for {
+		select {
+		case sum := <-res:
+			nonces = c.getAll(sum)
+			for _, v := range nonces {
+				c.header.doHash(v)
+			}
+			return
+		case <-stop:
+			log.Println("collider stopped")
+			return nil
+		case <-progress:
+			count++
+			log.Println("tried", count * 1000000, "nonces")
+		}
+	}
+}
